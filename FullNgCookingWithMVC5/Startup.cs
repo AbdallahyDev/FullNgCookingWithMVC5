b7cc7e58b895681b8b0a9c8b4cb1d6da67b556ff
abdallahyDev <abdallahy.bilal@gmail.com>
2016-06-13 14:12:53 +0200

    Ajoutez des fichiers projet.

---
+++ FullNgCookingWithMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FullNgCookingWithMVC5.Startup))]
+namespace FullNgCookingWithMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
